1a0897f8da543d86dc863593f08a138d8a0ffdc3
OlegT <koilanetroc@gmail.com>
2019-06-30 11:40:07 +0700

    added teacher rout

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -3,4 +3,11 @@
 class PageController < ApplicationController
   def index
   end
+
+  def teacher
+    if params[:password] != ENV["TEACHER_PASSWORD"]
+      redirect_to root_path
+    end
+    render "overview"
+  end
 end
